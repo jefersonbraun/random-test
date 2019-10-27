980d6c12b34becdf68279ffee424653274d21cb4
Jeferson B. Braun <jefersonbbraun@gmail.com>
2019-10-27 18:27:12 -0300

    Ã to $atilde;

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var things = ['Sim', 'Não', 'Talvez'];
+var things = ['Sim', 'N&atilde;o', 'Talvez'];
 var thing = things[Math.floor(Math.random()*things.length)];
 document.getElementById("vou").innerHTML = thing;
 document.getElementById("bodi").className = thing;
